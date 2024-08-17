334f06ee70180d5e9b1bd26c8e364b0b4480d551
fathimafida <fidafirozkattungal@gmail.com>
2024-08-17 16:31:40 +0530

    done

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,11 @@
 @extends ('layouts.app')
 
 
-
 @section  ('content')
 
 <div clsss="container mx-auto py-6">
 <div class="text-center bg-slate-100 ">
-<h1 class="text-3xl font-bold">Home</h1>
+<h1 class="text-3xl font-bold">About</h1>
 </div>
 
 </div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <title>Laravel</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="bg-gray-100">
+       <nav className="bg-white shadow-lg p-4">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+     <div className="flex space-x-4">
+     <a href="/" className=" hover:text-gray-400 py-5 px-3">Home</a>
+     <a href="/about" className=" hover:text-gray-400  py-5 px-3">About</a>
+     </div>
+</div>
+
+
+
+</div>
+    </nav>
+        <div class="container mx-auto py-6 ">
+           @yield('content')
+        </div>
+    </body>
+</html>
